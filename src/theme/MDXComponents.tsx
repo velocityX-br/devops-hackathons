a2104a0d27b4f4ca411c10a293d8c0178971fa8a
i577081 <bryan.chen01@sap.com>
2026-05-01 02:51:35 +0200

    FIX: nodejs version and pipeline failure

---
+++ src/theme/MDXComponents.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+import TrackLink from '@site/src/components/TrackLink';
+
+export default {
+  ...MDXComponents,
+  TrackLink,
+};
